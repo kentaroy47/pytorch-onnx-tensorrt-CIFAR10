241c59f8886ed886ac8ae4e915601257aa469828
kentaroy47 <kentaroy@stanford.edu>
2019-10-25 00:49:17 +0000

    init

---
+++ models/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from models.vgg import *
+#from dpn import *
+#from lenet import *
+#from senet import *
+#from pnasnet import *
+from models.densenet import *
+#from googlenet import *
+#from models.shufflenet import *
+from models.resnet import *
+from models.resnext import *
+#from preact_resnet import *
+#from mobilenet import *
+from models.mobilenetv2 import *
